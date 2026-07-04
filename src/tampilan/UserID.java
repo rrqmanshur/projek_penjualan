9ab7fba17085490899655dd5f697f80c071de01d
NibrasDaffa <daffakace@gmail.com>
2026-07-05 00:31:31 +0700

    push projek pribadi

---
+++ src/tampilan/UserID.java
@@ -0,0 +1,13 @@
+package tampilan;
+
+public class UserID {
+    private static String kd;
+
+    public static void setUserLogin(String kode) {
+        kd = kode;
+    }
+
+    public static String getUserLogin() {
+        return kd;
+    }
+}
